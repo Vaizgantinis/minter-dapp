d7780e10f2c2c6776013e397578e98609c84f94e
Vaizgantinis <100233106+Vaizgantinis@users.noreply.github.com>
2022-03-30 15:56:48 -0600

    updated presale witelist addresses

--- backend/src/config.js
+++ backend/src/config.js
@@ -67,7 +67,7 @@ const ROYALTY_SHARE = 1000; // Percentage of the token price that goes to the ro
 const ROYALTY_ADDRESS = "0xA6203147B068AA178ebd750f97D3D39C23faf4E7"; // Address that will receive the royalty
 const BASE_URI = null; // only update if you want to manually set the base uri
 const PREREVEAL_TOKEN_URI = null; // only update if you want to manually set the prereveal token uri
-const PRESALE_WHITELISTED_ADDRESSES = ["0xA6203147B068AA178ebd750f97D3D39C23faf4E7"]; // only update if you want to manually set the whitelisted addresses
+const PRESALE_WHITELISTED_ADDRESSES = []; // only update if you want to manually set the whitelisted addresses
 
 // ** OPTIONAL **
 let CONTRACT_ADDRESS = "YOUR CONTRACT ADDRESS"; // If you want to manually include it
